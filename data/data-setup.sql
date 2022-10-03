50120c06a813f03b0e5634f0b28d06d7a59a91c4
JeffRisberg <j.s.risberg@gmail.com>
2022-10-02 23:57:16 -0700

    added messages table.

--- data/data-setup.sql
+++ data/data-setup.sql
@@ -1,44 +1,65 @@
-USE docker02
+USE docker02;
 
 DROP TABLE IF EXISTS incident;
 
-CREATE TABLE incident (
-  id MEDIUMINT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NULL,
-  severity VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE incident
+(
+    id          MEDIUMINT    NOT NULL AUTO_INCREMENT,
+    title       VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NULL,
+    severity    VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 INSERT INTO incident(title, description, severity)
-VALUES('Account update required',
-       'Click on Update link, follow instructions',
-       'Medium');
+VALUES ('Account update required',
+        'Click on Update link, follow instructions',
+        'Medium');
 
 INSERT INTO incident(title, description, severity)
-VALUES('No response within timeout interval',
-       'Check the network wiring, check for firewall configuration',
-       'High');
+VALUES ('No response within timeout interval',
+        'Check the network wiring, check for firewall configuration',
+        'High');
 
 INSERT INTO incident(title, description, severity)
-VALUES('Unable to install new application',
-       'Check your password for the Apple App Store',
-       'Medium');
+VALUES ('Unable to install new application',
+        'Check your password for the Apple App Store',
+        'Medium');
 
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-  id MEDIUMINT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NULL,
-  PRIMARY KEY (id)
+CREATE TABLE users
+(
+    id         MEDIUMINT    NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NULL,
+    PRIMARY KEY (id)
 );
 
 INSERT INTO users(first_name, last_name)
-VALUES('Jack', 'Jones');
+VALUES ('Jack', 'Jones');
 
 INSERT INTO users(first_name, last_name)
-VALUES('Luke', 'Skywalker');
+VALUES ('Luke', 'Skywalker');
 
 INSERT INTO users(first_name, last_name)
-VALUES('Han', 'Solo');
+VALUES ('Han', 'Solo');
+
+DROP TABLE IF EXISTS messages;
+
+CREATE TABLE messages
+(
+    id     MEDIUMINT    NOT NULL AUTO_INCREMENT,
+    sender VARCHAR(255) NOT NULL,
+    text   TEXT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO messages(sender, text)
+VALUES ('Jack', 'Hello there');
+
+INSERT INTO messages(sender, text)
+VALUES ('Jill', 'let\'s fetch some water');
+
+INSERT INTO messages(sender, text)
+VALUES ('Jack', 'ok');
